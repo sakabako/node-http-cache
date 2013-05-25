11a79361a9fb47b8c222ee1b4166c86cac6c5ece
Stu Kabakoff <sakabako@gmail.com>
2013-05-24 23:32:50 -0400

    give cacheDurationFunciton the response time, set some defaults

--- http.js
+++ http.js
@@ -3,7 +3,18 @@ var http = require('http');
 
 var caches = {};
 
+function defaultHashingFunction( req ) {
+	return req.url;
+}
+function defaultCacheDurationFunction( req, responseTime ) {
+	return 0;
+}
+
 exports.createServer = function( callback, hashingFunction, cacheDurationFunction ) {
+
+	hashingFunction = hashingFunction || defaultHashingFunction;
+	cacheDurationFunction = cacheDurationFunction || defaultCacheDurationFunction;
+
 	var caches = {};
 	
 	return http.createServer(function (req, res) {
@@ -13,12 +24,14 @@ exports.createServer = function( callback, hashingFunction, cacheDurationFunctio
 			caches[hash].pipe(res);
 			return;
 		} else {
+			var startTime = new Date();
 			caches[hash] = new CachedResponse();
 			caches[hash].pipe(res);
 			caches[hash].on('end', function() {
+				var responseTime = new Date() - startTime;
 				setTimeout(function() {
 					delete caches[hash];
-				}, cacheDurationFunction(req) );
+				}, cacheDurationFunction(req, responseTime) );
 			});
 		}
 		callback( req, caches[hash] );
